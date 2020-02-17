c25f4e070ee6e1349c44dbc026d713b2963910b3
José Rogério <joserogerio.net@gmail.com>
2020-02-16 21:06:13 -0300

    Updates

---
+++ precache-manifest.82ef5e7b2976b40fbde8cabbc5bb8507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94db3f49e19d8f0c42e5666e7e270929",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "b8c0b03e87765e561bd7",
+    "url": "/react-gh-pages/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "4bc750e56107cfb63a56",
+    "url": "/react-gh-pages/static/css/main.d715ba1b.chunk.css"
+  },
+  {
+    "revision": "b8c0b03e87765e561bd7",
+    "url": "/react-gh-pages/static/js/2.453b7e2c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-gh-pages/static/js/2.453b7e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc750e56107cfb63a56",
+    "url": "/react-gh-pages/static/js/main.b0d32fa3.chunk.js"
+  },
+  {
+    "revision": "b8b12e1f1db15b7c0e68",
+    "url": "/react-gh-pages/static/js/runtime-main.0a2be737.js"
+  }
+]);
