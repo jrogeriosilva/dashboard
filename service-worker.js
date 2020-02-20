f9e62e94a2192b6ef40303a7b590ecd1fd03ef0f
José Rogério <joserogerio.net@gmail.com>
2020-02-19 21:22:47 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.99c8904245aec2a728ee922362239ba3.js"
+  "/dashboard/precache-manifest.8756d44da411f64505b129caf98e1ff4.js"
 );
 
 self.addEventListener('message', (event) => {
